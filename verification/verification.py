265aa6edff22b99665c53cd347cacee1ba9c8193
trumoose <xacriimony@gmail.com>
2022-01-08 04:53:07 -0600

    embed test

--- verification/verification.py
+++ verification/verification.py
@@ -22,31 +22,13 @@ class Verification(commands.Cog):
         async def callback(interaction):
             await interaction.send(content="Yay")
 
-        await ctx.send("goomp", components = [[
-                                                    self.bot.components_manager.add_callback(Button(style=ButtonStyle.green, label="Approve"), callback),
-                                                    self.bot.components_manager.add_callback(Button(style=ButtonStyle.red, label="Banhammer"), callback)
-                                               ]],
-        )
+        e = discord.Embed(description = f"**moosey#9999 has joined the server.")
+        e.add_field(name=_("Joined Discord on"), value="June 15, 2016 11:32 AM\n(6 years ago")
+        e.add_field(name=_("Joined this server on"), value="January 24, 2021 6:47 AM\n(a year ago)")
+        e.set_footer(text=_("Member #1 | User ID: 192677766003556352"))
+        e.set_author(name="moosey#9999", url="https://cdn.discordapp.com/avatars/192677766003556352/1c1bbd93c523d443bd3acc4ad2e525a3.png?size=1024")
+        e.set_thumbnail(url="https://cdn.discordapp.com/avatars/192677766003556352/1c1bbd93c523d443bd3acc4ad2e525a3.png?size=1024")
 
-    @checks.mod_or_permissions(administrator=True)
-    @commands.guild_only()
-    @commands.command()
-    async def select(self, ctx):
-        async def callback(interaction):
-            await interaction.send(content="Yay")
-
-        await ctx.send(
-            "Select callbacks!",
-            components=[
-                self.bot.components_manager.add_callback(
-                    Select(
-                        options=[
-                            SelectOption(label="a", value="a"),
-                            SelectOption(label="b", value="b"),
-                        ],
-                    ),
-                    callback,
-                )
-            ],
-        )
+        await ctx.send(embed = e, components = [[self.bot.components_manager.add_callback(Button(style=ButtonStyle.green, label="Approve"), callback),
+                                                 self.bot.components_manager.add_callback(Button(style=ButtonStyle.red, label="Banhammer"), callback)]])
 
