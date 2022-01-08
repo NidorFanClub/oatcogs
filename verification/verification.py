9410fa34bd7a29f66fa9601ffb2732d0b696e1a1
trumoose <xacriimony@gmail.com>
2022-01-08 06:05:16 -0600

    event

--- verification/verification.py
+++ verification/verification.py
@@ -15,11 +15,15 @@ class Verification(commands.Cog):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=1312420691312, force_registration=True)
 
+    @bot.event
+    async def on_button_click(interaction):
+        await interaction.respond(content="Button Clicked")
+
     @checks.mod_or_permissions(administrator=True)
     @commands.guild_only()
     @commands.command()
     async def button(self, ctx):
         await ctx.send("i love my vegan friends. especially muradok", components = [[self.bot.components_manager.add_callback(Button(style = ButtonStyle.green, label = "Approve", custom_id = "approve")),
-                                                 self.bot.components_manager.add_callback(Button(style = ButtonStyle.grey, emoji = bot.get_emoji(929343381409255454), custom_id = "sus"))
-                                                 self.bot.components_manager.add_callback(Button(style = ButtonStyle.red, label = "Ban", custom_id = "ban"))]])
+                                                                                    self.bot.components_manager.add_callback(Button(style = ButtonStyle.grey, emoji = bot.get_emoji(929343381409255454), custom_id = "sus"))
+                                                                            self.bot.components_manager.add_callback(Button(style = ButtonStyle.red, label = "Ban", custom_id = "ban"))]])
 
