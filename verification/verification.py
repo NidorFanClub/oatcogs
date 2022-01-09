80c7a487679efea2d0589bddc7504350dcf4c246
trumoose <xacriimony@gmail.com>
2022-01-08 18:46:43 -0600

    button testing

--- verification/verification.py
+++ verification/verification.py
@@ -24,7 +24,7 @@ class Verification(commands.Cog):
             await ctx.send("hai")
             print(TEST)
             for button in buttons:
-                if button.id is not "lock":
+                if button.id != "lock":
                     button.disabled = not button.disabled
                 else:
                     button.emoji = "🔒"
