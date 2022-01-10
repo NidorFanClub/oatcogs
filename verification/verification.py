09e9719bf6eddc703e45e230c079287c4a6d2d15
trumoose <xacriimony@gmail.com>
2022-01-10 08:04:32 -0600

    onjoin test

--- verification/verification.py
+++ verification/verification.py
@@ -146,7 +146,7 @@ class Verification(commands.Cog):
     async def on_button_click(self, interaction):
         buttons = interaction.message.components
 
-        member = self.get_user(interaction.message)
+        member = await self.get_user(interaction.message)
 
         if not member:
             return
