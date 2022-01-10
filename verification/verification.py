762ba82ee1cd811ab7ac14a362bb5b7e63d305a4
trumoose <xacriimony@gmail.com>
2022-01-10 07:01:14 -0600

    onjoin test

--- verification/verification.py
+++ verification/verification.py
@@ -18,25 +18,25 @@ class Verification(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=1312420691312, force_registration=True)
-        self.config.register_guild(verifier_channel = None, cached_users = {})
-
-    global invites = {}
+        self.config.register_guild(verifier_channel = None, cached_users = {}, invites = {})
 
     async def update_invites(self, guild: discord.Guild):
-        invites[guild.id] = await guild.invites()
+        async with self.config.guild(guild).invites() as invites:
+            for invite in await guild.invites():
+                invites[invite.code] = invite.uses
 
     async def find_invite(self, guild: discord.Guild):
         invites_after_join = await guild.invites()
-        invites_before_join = invites[guild.id]
+        invites_before_join = await self.config.guild(guild).invites()
 
         print(f"{invites_before_join}")
 
         for invite_after in invites_after_join:
             print(f"invite_after join: {invite_after.code}, {type(invite_after.code)}", flush=True)
-            for invite_before in invites_before_join:
-                print(f"invite_before join: {invite_before.code}, {type(invite_before.code)}", flush=True)
-                if invite_before.code == invite_after.code:
-                    if invite_before.uses < invite_after.uses:
+            for invite_before_code, invite_before_uses in invites_before_join.items():
+                print(f"invite_before join: {invite_before_code}, {type(invite_before_uses)}", flush=True)
+                if invite_before_code == invite_after.code:
+                    if invite_before_uses < invite_after.uses:
                         print("match for the invite!", flush=True)
                         return invite_before
                     else:
