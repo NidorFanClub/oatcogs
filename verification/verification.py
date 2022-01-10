a0a11035881c3bee1e8b943ad4d1c8b5f72e0034
trumoose <xacriimony@gmail.com>
2022-01-10 09:48:51 -0600

    onjoin test

--- verification/verification.py
+++ verification/verification.py
@@ -201,7 +201,7 @@ class Verification(commands.Cog):
 
         for message_id in cached_users[f"{member.id}"]:
             channel = discord.utils.get(member.guild.channels, id = int(await self.config.guild(member.guild).verifier_channel()))
-            await channel.fetch_message(message_id)
+            message = await channel.fetch_message(message_id)
             await message.edit_origin(components = new_buttons)
 
 
