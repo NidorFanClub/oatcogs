cf053236d76d3af6dfcce7c39df43c6fcf34aae6
trumoose <xacriimony@gmail.com>
2022-01-10 05:52:56 -0600

    onjoin test

--- verification/verification.py
+++ verification/verification.py
@@ -18,7 +18,7 @@ class Verification(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=1312420691312, force_registration=True)
-        self.config.register_guild(verifier_channel = None, cached_users = {}, invites = [])
+        self.config.register_guild(verifier_channel = None, cached_users = {}, invites = {})
 
     async def find_invite(self, member: discord.Member):
         invites_after_join = await member.guild.invites()
