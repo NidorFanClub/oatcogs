3b4586912df54d1f31f243fadd2a6eb4809fd852
trumoose <xacriimony@gmail.com>
2022-01-10 06:19:20 -0600

    onjoin test

--- verification/verification.py
+++ verification/verification.py
@@ -24,7 +24,7 @@ class Verification(commands.Cog):
         invites_after_join = await member.guild.invites()
 
         async with self.config.guild(member.guild).cached_invites() as cached_invites:
-            print(f"{cached_invites}", flush=True)
+            print(f"{member.guild.invites()}", flush=True)
             for invite in cached_invites:
                 print(f"invite_before join: {invite.code}, {type(invite.code)}", flush=True)
                 for invite_after in invites_after_join:
