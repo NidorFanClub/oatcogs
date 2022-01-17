883f5d686db45155a087547606f8c07baf1325e5
trumoose <xacriimony@gmail.com>
2022-01-17 09:33:59 -0600

    Fixed fstring formatting

--- wordle/wordle.py
+++ wordle/wordle.py
@@ -379,7 +379,7 @@ class Wordle(commands.Cog):
             magnitude += 1
             num /= 1000.0
 
-        return f"{"{num:f}".rstrip('0').rstrip('.')}{['', 'K', 'M', 'B', 'T'][magnitude]}"
+        return f"{num:f.rstrip('0').rstrip('.')}{['', 'K', 'M', 'B', 'T'][magnitude]}"
 
     @commands.command()
     @commands.guild_only()
