6aebab6331ff5f57b81af2d2ef53c5e1cb4de341
trumoose <xacriimony@gmail.com>
2022-01-18 02:57:48 -0600

    Added .avatar command

--- moosetools/moosetools.py
+++ moosetools/moosetools.py
@@ -9,7 +9,7 @@ import os
 import typing
 
 class moosetools(commands.Cog):
-    """various tools that don't deserve their own cog."""
+    """various commands that don't deserve their own cog."""
     def __init__(self):
         self.config = Config.get_conf(self, identifier=13121311231233, force_registration=True)
 
@@ -17,6 +17,9 @@ class moosetools(commands.Cog):
     @commands.guild_only()
     @commands.command()
     async def member_ids(self, ctx):
+        """
+        Returns all member IDs in the server as a formatted list of
+        """
         with open("member_ids.txt", "w") as file:
             for user in ctx.guild.members:
                 file.write(str(user.id) + "\n")
@@ -24,3 +27,20 @@ class moosetools(commands.Cog):
         with open("member_ids.txt", "rb") as file:
             await ctx.send(file=discord.File(file, "member_ids.txt"))
             await ctx.tick()
+
+    @commands.command()
+    @commands.guild_only()
+    async def avatar(self, ctx, member: discord.Member):
+        """
+        Returns a member's avatar.
+        """
+        if not member:
+            member = ctx.author
+
+        if member.is_avatar_animated():
+            avatar = member.avatar_url_as(format="gif")
+        if not member.is_avatar_animated():
+            avatar = member.avatar_url_as(static_format="png")
+
+        await ctx.send(f"{avatar}")
+
