65478f1c12c7bf1b479d7cb3b1a810eb73ee0107
trumoose <xacriimony@gmail.com>
2022-01-19 19:31:20 -0600

    Formatting

--- anonpoll/__init__.py
+++ anonpoll/__init__.py
@@ -2,6 +2,5 @@ from .anonpoll import AnonPoll
 
 __red_end_user_data_statement__ = "This cog stores Discord IDs as needed for operation temporary during a poll which are automatically deleted when it ends."
 
-
 def setup(bot):
     bot.add_cog(AnonPoll(bot))
