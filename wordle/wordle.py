03c78b9e6c6f5a40c80cfabc4d36dc6f42e37faa
trumoose <xacriimony@gmail.com>
2022-01-26 17:37:26 -0600

    IM SO BAD AT THIS

--- wordle/wordle.py
+++ wordle/wordle.py
@@ -92,7 +92,7 @@ class Wordle(commands.Cog):
 
                     old_message = message
 
-                    if guess.content.lower() != target_word
+                    if guess.content.lower() != target_word:
                         message = await ctx.send(file=wordle_file)
 
                     try:
