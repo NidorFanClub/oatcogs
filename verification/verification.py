8ff3b14b19eb51e00f9a162526673dcca06fe535
trumoose <xacriimony@gmail.com>
2022-01-30 22:15:37 -0600

    PLEASE TELL ME ITS FIXED

--- verification/verification.py
+++ verification/verification.py
@@ -205,14 +205,11 @@ class Verification(commands.Cog):
         buttons = interaction.message.components
         guild = interaction.guild
 
-        print("Clicked button", flush=True)
         user_id = int(await self.get_user(interaction.message))
         member = guild.get_member(user_id)
         if not member:
-            print("Couldn't find member", flush=True)
-            member = await self.bot.fetch_user(user_id)
+            user = await self.bot.fetch_user(user_id)
 
-        print("Got past member", flush=True)
         verifier = False
         verifier_roles = await self.config.guild(guild).verifier_roles()
         approved_roles = await self.config.guild(guild).approved_roles()
@@ -227,12 +224,13 @@ class Verification(commands.Cog):
         if not await self.bot.is_owner(interaction.user) and not verifier:
             return
 
-        for approved_role_id in approved_roles:
-            role = discord.utils.get(guild.roles, id=int(approved_role_id))
-            if role in member.roles:
-                new_buttons = [[Button(style=ButtonStyle.green, label=f"Approved", custom_id="approve", disabled=True)]]
-                await interaction.edit_origin(components=new_buttons)
-                return
+        if member:
+            for approved_role_id in approved_roles:
+                role = discord.utils.get(guild.roles, id=int(approved_role_id))
+                if role in member.roles:
+                    new_buttons = [[Button(style=ButtonStyle.green, label=f"Approved", custom_id="approve", disabled=True)]]
+                    await interaction.edit_origin(components=new_buttons)
+                    return
 
         cached_users = await self.config.guild(guild).cached_users()
 
@@ -281,7 +279,6 @@ class Verification(commands.Cog):
                             Button(style=ButtonStyle.red, label="Unban", custom_id="unban_check", disabled=False)]]
 
         elif interaction.custom_id == "unban_check":
-            print("In unban check", flush=True)
             new_buttons = [[Button(style=ButtonStyle.green, label="Confirm unban?", custom_id="unban", disabled=False),
                             Button(style=ButtonStyle.red, label="Cancel", custom_id="cancel", disabled=False)]]
 
