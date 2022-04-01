7f9d193271cda1231aa2b964b5f52cedc41b65f0
trumoose <xacriimony@gmail.com>
2022-04-01 05:07:11 -0500

    works for edits too yay

--- april/april.py
+++ april/april.py
@@ -22,12 +22,12 @@ class April(commands.Cog):
         valid_user = isinstance(author, discord.Member) and not author.bot
         if not valid_user:
             return
-        channels = [926112975813750796,927783621794877460,926113551968526376,926113675419471972,927518938919735326,927518938919735326,927539973459169302,928689945080627201,930531314363424808]
+        channels = [926112975813750796, 927783621794877460, 926113551968526376, 926113675419471972,
+                    927518938919735326, 927518938919735326, 927539973459169302, 928689945080627201, 930531314363424808]
         if message.channel.id in channels:
 
-
             if len(message.attachments) == 0:
-                x = re.search(r'^<a.*:|<:.*>$',msg.content)
+                x = re.search(r'^<a.*:|<:.*>$', msg.content)
                 if not x:
                     valid = False
                 else:
@@ -42,15 +42,18 @@ class April(commands.Cog):
 
                             valid2 = False
                         else:
-                            i = msg.content.replace(symbol,'')
-                            x = re.search(r'^\s*<:.*>\s*$',str(i))
+                            i = msg.content.replace(symbol, '')
+                            x = re.search(r'^\s*<:.*>\s*$', str(i))
                             if x:
                                 valid = True
                                 valid2 = True
-
 
             if valid == False and valid2 == False:
                 try:
                     await message.delete()
                 except discord.HTTPException:
                     pass
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, _prior, message):
+        await self.on_message(message)
